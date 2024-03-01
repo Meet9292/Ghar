efcd227bbf75afabe55baec94f33ec78db89ee41
Meet9292 <21cs023@charusat.edu.in>
2024-03-01 14:49:08 +0530

    create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
+// Sign api route
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -15,5 +18,35 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error)
   } 
-
 };
+
+// Sign in api route
+export const signin = async (req,res,next)=>{
+  const {email,password} = req.body
+
+  try {
+    const validuser = await User.findOne({email})
+    if(!validuser)
+    {
+      return next(errorHandler(404,'User not Found!'))
+    }
+    const validPassword = bcryptjs.compareSync(password,validuser.password)
+    if(!validPassword)
+    {
+      return next(errorHandler(401,'Password Incorrect!'))
+    }
+
+    const token = jwt.sign({id : validuser._id},process.env.JWT_SECRET)
+
+    //Hides Password to user
+    const {password: pass,...rest} = validuser._doc
+
+    res
+    .cookie('access_token',token,{httpOnly:true})
+    .status(200)
+    .json(rest)
+
+  } catch (error) {
+    next(error)
+  }
+}
