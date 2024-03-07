9d8cf8e8132b10f08637b4ab5bdd7f8c5d11cf74
Meet9292 <21cs023@charusat.edu.in>
2024-03-07 21:33:56 +0530

    Complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile'
 import Header from './components/Header'
 import Signup from './pages/Signup'
 import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
 
 export default function App()
 {
@@ -20,6 +21,7 @@ export default function App()
     <Route element={<PrivateRoute/>}> 
       <Route path='/profile' element={<Profile/>} />
     </Route>
+    <Route path='/create-listing' element={<CreateListing/>} />
   </Routes>
   </BrowserRouter>
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
   signOutFailure,
   signOutSuccess
 } from "../redux/user/userSlice";
+import {Link} from 'react-router-dom'
 
 function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -190,6 +191,11 @@ function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+
+        <Link className="bg-green-700 text-white p-3 rounded-lg text-center uppercase hover:opacity-90" to={'/create-listing'}>
+        Create Listing
+        </Link>     
+
       </form>
 
       <div className="flex justify-between mt-5">
