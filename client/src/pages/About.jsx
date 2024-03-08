cf195047e9642d0b1f60f31050c00accc0cd5834
Meet9292 <21cs023@charusat.edu.in>
2024-03-09 02:47:27 +0530

    Complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-600'>
+        About Ghar
+      </h1>
+      <p className='text-slate-700 mb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint dolore nesciunt, omnis voluptatem nemo eaque. Ipsa ea quaerat repellat rem deleniti aliquid, beatae maxime ducimus inventore quis omnis blanditiis, officiis earum atque aperiam quod error distinctio quo accusamus maiores unde quibusdam magnam! Cum deserunt totam magni veritatis atque, delectus aspernatur.</p>
+      <p className='text-slate-700 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem quia voluptates, maiores mollitia consequatur quo itaque nulla corrupti consectetur cum ipsa id quaerat, tenetur quibusdam illo. Accusamus, inventore neque!</p>
+      <p className='text-slate-700 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus dolorem ut quaerat sed doloribus! Possimus.</p>
+    </div>
   )
 }
 
