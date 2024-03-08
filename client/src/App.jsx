ece6c3596ba66c762ee4d16dbdb450050e6af29b
Meet9292 <21cs023@charusat.edu.in>
2024-03-08 14:40:21 +0530

    complete update listing functionality client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Signup from './pages/Signup'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
 
 export default function App()
 {
@@ -20,8 +21,9 @@ export default function App()
     <Route path='/about' element={<About/>} />
     <Route element={<PrivateRoute/>}> 
       <Route path='/profile' element={<Profile/>} />
+      <Route path='/create-listing' element={<CreateListing/>} />
+      <Route path='/update-listing/:listingId' element={<UpdateListing/>} />
     </Route>
-    <Route path='/create-listing' element={<CreateListing/>} />
   </Routes>
   </BrowserRouter>
 }
